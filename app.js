e6a8a4a1dc44a7bfa15d152721d28d3352880513
zteplez <asibrahim98@gmail.com>
2025-04-09 15:43:52 +0300

    feat: Add addBookLibrary function and fix printInfo method

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const library = [];
 
 function Book(name, pages, author, isRead){
+    this.id = crypto.randomUUID();
     this.name = name;
     this.pages = pages;
     this.author = author;
     this.isRead = isRead;
     this.printInfo = function(){
-        console.log(`Book information -> Name: ${name} pages: ${pages}, author: ${author}`);
+        console.log(`Book information -> Id: ${this.id} Name: ${this.name} pages: ${this.pages}, author: ${this.author}`);
     }
 }
 
-// Example book
-let book1 = new Book("Hobbit", 324, "Tolkien", false);
-
-book1.printInfo();
+function addBookLibrary(name, pages, author, isRead){
+    let newBook = new Book(name, pages, author, isRead);
+    library.push(newBook);
+    console.log("Add new book.");
+}
